1fbb383d319f86dd773efd549cc4125d53bbabfe
calebyvan <calebyvan1@gmail.com>
2025-05-28 15:41:09 +0200

    Update main.py

--- P1/P1C4/énoncé/main.py
+++ P1/P1C4/énoncé/main.py
@@ -1,1 +1,11 @@
 # Écrivez votre code ici !
+# érire un nom 
+nom = Caleb
+# écrire un age 
+àge = 25
+# afficher une phrase
+print(f" Je m'appelle " {nom}  et j'ai {age} ans ")
+# modifier l'àge
+age = 25 + 10
+# afficher une nouvelle phrase
+print(f" Je m'appelle " {nom}  et j'ai {age} ans ")
